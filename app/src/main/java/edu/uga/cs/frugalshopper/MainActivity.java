494939dea4f756cb03a06f9f3880e57dee01eb15
Jacob Gil <jacobgil325@gmail.com>
2022-02-03 11:02:49 -0500

    adding ids to java file

--- app/src/main/java/edu/uga/cs/frugalshopper/MainActivity.java
+++ app/src/main/java/edu/uga/cs/frugalshopper/MainActivity.java
@@ -3,12 +3,41 @@ package edu.uga.cs.frugalshopper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String DEBUG_TAG = "FrugalShopper";
+
+    private EditText priceA;
+    private EditText poundsA;
+    private EditText ouncesA;
+    private EditText priceB;
+    private EditText poundsB;
+    private EditText ouncesB;
+    private EditText priceC;
+    private EditText poundsC;
+    private EditText ouncesC;
+    private TextView costBestProduct;
+    private Button compare;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i( DEBUG_TAG, "MainActivity.onCreate()" );
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        compare = findViewById( R.id.button );
+        compare.setOnClickListener( new ButtonClickListener() );
+    }
+
+    private class ButtonClickListener implements View.OnClickListener
+    {
+
     }
 }
